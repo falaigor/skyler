ae6cf6d12dbb923bde6d7574311d8d790cfa3ca5
Igor Santos <falaigors@gmail.com>
2021-10-29 23:45:21 -0300

    icons / colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -9,18 +9,24 @@ export const theme = extendTheme({
   config,
   colors: {
     gray: {
+      "900": "#4A4C50",
       "500": "#40476C",
       "10": "#F0F5FF",
     },
     blue: {
+      "900": "#282D46",
+      "700": "#40476C",
+      "500": "#6A8FDA",
       "200": "#81ABFF",
       "100": "#9DBEFF",
     },
     pink: {
+      "500": "#DE5F8E",
       "200": "#FF71A5",
       "100": "#F89FC4",
     },
     orange: {
+      "500": "#E19845",
       "200": "#FFB35C",
       "100": "#FFC480",
     },
